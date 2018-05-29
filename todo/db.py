96c22b372880f536ba941f6dd55108f7dd5cc025
Your Name <yh4105@ubuntu.ubuntu-domain>
2018-05-29 21:34:33 +0800

    add bd.py

---
+++ todo/db.py
@@ -0,0 +1,9 @@
+class TodoDB(object):
+
+    def read_all(self):
+        return ["测试1",
+                "测试2",
+                "测试3",
+                "测试4",
+                "测试5",
+                "测试6"]
