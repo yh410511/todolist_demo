43b15a6a5d3219209541708511681209603d6279
Your Name <yh4105@ubuntu.ubuntu-domain>
2018-05-30 20:37:18 +0800

    update

--- todo/db.py
+++ todo/db.py
@@ -1,9 +1,35 @@
+import sqlite3
+
+
 class TodoDB(object):
 
     def read_all(self):
-        return ["测试1",
-                "测试2",
-                "测试3",
-                "测试4",
-                "测试5",
-                "测试6"]
+        conn = sqlite3.connect('test.db')
+        cursor = conn.cursor()
+        cursor = cursor.execute('select content from todo')
+        data = cursor.fetchall()
+        data = [d[0] for d in data]
+        cursor.close()
+        conn.close()
+        return data
+
+        # return ["测试1",
+        #         "测试2",
+        #         "测试3",
+        #         "测试4",
+        #         "测试5",
+        #         "测试6"]
+
+    def init_db(self):
+        conn = sqlite3.connect('test.db')
+        cursor = conn.cursor()
+        cursor.execute('create table todo (id INTEGER primary key AUTOINCREMENT, content varchar(50))')
+        cursor.close()
+        conn.commit()
+        conn.close()
+
+
+if __name__ == "__main__":
+    db = TodoDB()
+    db.read_all()
+    # db.init_db()
