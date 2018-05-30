11aac0154d91f695275310f2d51cd11fa05116db
Your Name <yh4105@ubuntu.ubuntu-domain>
2018-05-30 20:39:19 +0800

    update main.py

--- todo/main.py
+++ todo/main.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+
+from todo.db import TodoDB
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return 'hello world'
+def index():
+    db = TodoDB()
+    todo = db.read_all()
+    return render_template('index.html', data=todo)
 
 
 if __name__ == "__main__":
