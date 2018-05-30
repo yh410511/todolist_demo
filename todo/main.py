0c23e8a51b1115678b6b336e4d1a966db4d02459
Your Name <yh4105@ubuntu.ubuntu-domain>
2018-05-30 21:36:32 +0800

    add delete()

--- todo/main.py
+++ todo/main.py
@@ -12,5 +12,12 @@ def index():
     return render_template('index.html', data=todo)
 
 
+@app.route('/todo/<int:todo_id>', methods=['DELETE'])
+def delete(todo_id):
+    db = TodoDB()
+    todo = db.delete(todo_id)
+    return 'ok'
+
+
 if __name__ == "__main__":
     app.run(debug=True)
